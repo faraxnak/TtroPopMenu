e2b6397b42a862721ed3cb4b797170871c832c50
faraxnak <farid@Farids-MacBook-Pro.local>
2016-12-27 19:16:32 +0330

    sample project removed

--- TtroPopMenuSample/Pods/Target Support Files/PayWandBasicElements/PayWandBasicElements-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PayWandBasicElements : NSObject
-@end
-@implementation PodsDummy_PayWandBasicElements
-@end
--- TtroPopMenuSample/Pods/Target Support Files/TtroPopMenu/TtroPopMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TtroPopMenu : NSObject
-@end
-@implementation PodsDummy_TtroPopMenu
-@end
